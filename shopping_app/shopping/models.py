7686d5cd7e2e048c31ef96b8a585239a5c28efda
Ndifreke <ndy40.ekott@gmail.com>
2022-12-21 23:04:11 +0000

    feat(shopping): Add sub_channel field to Shopping list
    - add `sub_channel` field to ShoppingList model for websocket subscription.
    
    closes #2

--- shopping_app/shopping/models.py
+++ shopping_app/shopping/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from accounts.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -37,9 +39,19 @@ class ShoppingList(models.Model):
         User, related_name="shared_with", through="SharedWith"
     )
 
+    sub_channel = models.UUIDField(null=True, blank=True)
+
     @property
     def title(self):
         return f"Shopping List {self.created_at.strftime('%Y %B %d')}"
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if not self.sub_channel:
+            self.sub_channel = uuid.uuid4()
+
+        super().save(force_insert, force_update, using, update_fields)
 
 
 class ShoppingItem(models.Model):
