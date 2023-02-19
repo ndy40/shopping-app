4a6aa0d84f0afcb1b3cf4e62523721238cf942df
Ndifreke Ekott <ndy40.ekott@gmail.com>
2023-02-19 22:57:49 +0000

    CI/CD - Adding github actions to run basic tests (#21)
    * build: CI/CD

---
+++ shopping_app/core/settings/ci.py
@@ -0,0 +1,16 @@
+from .settings import *  # noqa
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "shopping_app",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+
+ACCOUNT_EMAIL_VERIFICATION = None
